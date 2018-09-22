467caf7731577348850f66bf2a888029e739b492
tianhuan <tianhuantu@oakland.edu>
2018-09-22 12:06:31 -0400

    FEATURE: be able to read and extract data

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <stdlib.h>
-#include <stdio.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cstring>
 
 /*struct MyTuple {
     int address;
@@ -9,39 +10,30 @@
 
 int main() {
 
-//    void extract_data(int *operation_id, int *timestamp, float *read_timestamp, int *call_id);
-
-    const char *read_file = "../read_first_100_lines.out";
+    const char read_file[] = "../read_first_100_lines.out";
     FILE *read_single;
     char buffer[50];
-    char row[4][25];
-    int *operation_id, *address, *call_id, *times;
-    float *read_timestamp = 0;
-    char *cursor = "a";
-
+    int operation_id, address, call_id, times;
+    float read_timestamp;
+    char *buffer_string;
 
     // read the file
     read_single = fopen(read_file, "r");
-    if (read_single == NULL) {
+    if (read_single == nullptr) {
         printf("Reading %s failed!", read_file);
         exit(2);
     }
 
-//    char s[50];
-    for (int i = 0; i < 1 || cursor != NULL; i++) {
-        cursor = fgets(buffer, 50, read_single);
-        sscanf(buffer, "[\n]\n", buffer);
-        sscanf(buffer, "%s %s %s %s", row[0], row[1], row[2], row[3]);
-//        sprintf(s, "%d\t%f\t%x\t%d\n", row[0], row[1], row[2], row[3]);
-//        printf(s);
+    while (nullptr != (buffer_string = fgets(buffer, 50, read_single))) {
+        printf("%s", buffer_string);
+        sscanf(buffer_string, "%d %f %x %d", &operation_id, &read_timestamp, &address, &call_id);
+        printf("%d\t%.1f\t0x%08x\t%d\n", operation_id, read_timestamp, address, call_id);
+
+        printf("\n");
     }
 
     fclose(read_single);
 
     printf("Reading %s succeeded!\n", read_file);
     return 0;
-}
-
-void extract_data(char buffer[50]) {
-
 }
