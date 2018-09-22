505dbbe10e132f03464c8cb352f4485a5d253ab3
tianhuan <tianhuantu@oakland.edu>
2018-09-21 22:18:48 -0400

    FEATURE: reading the file succeed!

--- main.cpp
+++ main.cpp
@@ -1,23 +1,35 @@
-#include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
 
+
+/*struct MyTuple {
+    int address;
+    int id;
+    int times;
+};*/
+
 int main() {
-    char c[1000];
-    FILE *fptr;
 
-    if ((fptr = fopen("program.txt", "r")) == NULL)
-    {
-        printf("Error! opening file");
-        // Program exits if file pointer returns NULL.
-        exit(1);
+    const char *read_file = "read_first_100_lines.out";
+    FILE *read_single = fopen(read_file, "r");
+    char *buffer;
+    int *operation_id = 0, *address = 0, *call_id = 0, times = 0;
+    float *read_timestamp = 0;
+    if (read_single == NULL) {
+        printf("Reading %s failed!", read_file);
+        exit(2);
     }
+/*    while (EOF != fscanf(read_single, "%d %f %d %d", operation_id, read_timestamp, address, call_id)) {
+        printf("%d\t%f\t%d\t%d", *operation_id, *read_timestamp, *address, *call_id);
+    }*/
 
-    // reads text until newline
-    fscanf(fptr,"%[^\n]", c);
+    fclose(read_single);
 
-    printf("Data from the file:\n%s", c);
-    fclose(fptr);
-
+    printf("Reading %s succeeded!\n", read_file);
     return 0;
 }
+
+/*
+void extract_data(int *operation_id, int *timestamp, float *read_timestamp, int *call_id) {
+
+}*/
