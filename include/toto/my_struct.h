16726109c8f664e70d69cd766b6fd8191225c805
tianhuan <tianhuantu@oakland.edu>
2018-09-23 02:33:49 -0400

    FEATURE: add codes to count read_single.out with created function_id_map. my_struct, function_id_map, and sort_region need to be considered and re-implemented

--- include/toto/my_struct.h
+++ include/toto/my_struct.h
@@ -5,14 +5,17 @@
 #ifndef HOMEWORK1_TIANHUAN_TU_MYSTRUCT_H
 #define HOMEWORK1_TIANHUAN_TU_MYSTRUCT_H
 
+// data type for storing read records and write records
 typedef struct{
     int address;
     int id;
     int times;
 } MyStruct;
 
+// data type for storing region record
 typedef struct{
     int id;
     char function_name[50];
 } FunctionIdSet;
+
 #endif //HOMEWORK1_TIANHUAN_TU_MYSTRUCT_H
--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <cstdio>
+#include <bits/stdc++.h>
 
 #include <string>
 #include <map>
@@ -10,7 +11,9 @@
 #include "include/toto/read_the_single_file.h"
 #include "include/toto/read_the_region_file.h"
 
-typedef set<int>::iterator const_iterator;
+// data type for storing count result during caculation
+typedef map<string, int> count_map;
+
 using namespace std;
 
 template<class T>
@@ -20,9 +23,27 @@ bool freeMemorySpace(T *p) {
     }
 }
 
+/**
+ * this method is aimed at converting a string to a given char array
+ * @param s
+ * @param ch
+ * @return
+ */
+bool string_to_char_array(const string &s, char ch[]) {
+    if (sizeof(ch) < (s.length() + 1)) {
+        printf("char array is too samll to store the string");
+        return false;
+    }
+    sprintf(ch, "%s", s.c_str());
+    return true;
+}
+
 int main() {
+    string generate_key_of_count_map(int address, const string &name);
 
     // input files
+    // because I use CLion and the executable output directory is the cmake-build-debug.
+    // so the relative path should be ../XXX
     char read_single_file[] = "../read_first_100_lines.out";
     char write_single_file[] = "../write_first_100_lines.out";
     // the origin region.out has an unsupported first line. remove it with bash command "sed -i "1,1d" region.out"
@@ -39,12 +60,14 @@ int main() {
     unsigned long int number_of_region = 0;
 
     // read and extract data from files
-//    beginning_read_single = read_the_single_file(read_single_file, &number_of_read_single);
+    beginning_read_single = read_the_single_file(read_single_file, &number_of_read_single);
 //    beginning_write_single = read_the_single_file(write_single_file, &number_of_write_single);
     beginning_region = read_the_region_file(region_file, &number_of_region);
 
     function_id_map region_map;
     mergeRegion(beginning_region, number_of_region, &region_map);
+
+/*
 
     for (auto &it : region_map) {
         cout << it.first << "\t";
@@ -53,15 +76,70 @@ int main() {
         }
         cout << endl;
     }
+*/
 
 
     // do not forget to release the memory space to avoid memory leak!
-    freeMemorySpace(beginning_read_single);
-    freeMemorySpace(beginning_write_single);
+    // now the data from region.out in memory become useless
     freeMemorySpace(beginning_region);
 
+
+    // for each variable/address,
+    // to count the total number of read operation and write operation from the same instance call
+    // since the read record and write record has same structure.
+    // we could count in read record and write record sequentially first,
+    // and then go to multi-thread calculation
+
+    // to create an enormous Map to save result during the calculation
+    count_map count_result;
+    count_map::iterator cursor;
+    // temp variables used during counting
+    int address, call_id;
+    string function_name;
+
+
+    // for records from read_single file
+    for (int i = 0; i < number_of_read_single; i++) {
+        address = (beginning_read_single + i)->address;
+        call_id = (beginning_read_single + i)->id;
+        // first to find out the name of the function which called it
+        for (auto &k: region_map) {
+            if (k.second.find(call_id) != k.second.end()) {
+                // call_id = id
+                function_name = k.first;
+                break;
+            }
+        }
+
+        // secondly to make counter ++
+        string key = generate_key_of_count_map(address, function_name);
+        cursor = count_result.find(key);
+        if (cursor != count_result.end()) {
+            // if the key/value pair exists, increase it by 1
+            cursor->second++;
+        } else {
+            // if not exist, insert this new record
+            count_result.insert(pair<string, int>(key, 1));
+        }
+    }
+
+    char addr_string[20];
+    for (auto &k: count_result) {
+        sprintf(addr_string, "%s", k.first.c_str());
+        printf("%-50s%d\n", addr_string, k.second);
+    }
+
+
+    freeMemorySpace(beginning_read_single);
+    freeMemorySpace(beginning_write_single);
     return 0;
 }
+
+string generate_key_of_count_map(int address, const string &name) {
+    char key[60];
+    sprintf(key, "%08x_%s", address, name.c_str());
+    return (string) key;
+};
 
 
 
